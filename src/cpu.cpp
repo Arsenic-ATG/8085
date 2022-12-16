d95403a9c907dd5fbbf5b7d8eacf163378f4b58a
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-12-16 12:49:24 +0530

    Enh: implement MOV instruction set

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,1 +1,377 @@
-#include "cpu.hpp"
+#include "cpu.h"
+#include <iostream>
+
+using namespace emu;
+
+//  memeory
+
+void Memory::initialise() { data = {}; }
+
+// class cpu
+
+void CPU::reset(Memory &mem) {
+  pc = A = B = C = D = E = H = L = 0;
+  psw = {};
+  mem.initialise();
+}
+
+void CPU::execute(int &cycles, Memory &mem) {
+  while (cycles > 0) {
+    auto instruction = fetchByte(cycles, mem);
+    switch (instruction) {
+
+    case MOV_AA:
+    case MOV_BB:
+    case MOV_CC:
+    case MOV_DD:
+    case MOV_EE:
+    case MOV_HH:
+    case MOV_LL:
+      break;
+
+    case MOV_AB: {
+      moveRegister(A, B);
+    } break;
+    case MOV_AC: {
+      moveRegister(A, C);
+    } break;
+    case MOV_AD: {
+      moveRegister(A, D);
+    } break;
+    case MOV_AE: {
+      moveRegister(A, E);
+    } break;
+    case MOV_AH: {
+      moveRegister(A, H);
+    } break;
+    case MOV_AL: {
+      moveRegister(A, L);
+    } break;
+
+    case MOV_BA: {
+      moveRegister(B, A);
+    } break;
+    case MOV_BC: {
+      moveRegister(B, C);
+    } break;
+    case MOV_BD: {
+      moveRegister(B, D);
+    } break;
+    case MOV_BE: {
+      moveRegister(B, E);
+    } break;
+    case MOV_BH: {
+      moveRegister(B, H);
+    } break;
+    case MOV_BL: {
+      moveRegister(B, L);
+    } break;
+
+    case MOV_CA: {
+      moveRegister(C, A);
+    } break;
+    case MOV_CB: {
+      moveRegister(C, B);
+    } break;
+    case MOV_CD: {
+      moveRegister(C, D);
+    } break;
+    case MOV_CE: {
+      moveRegister(C, E);
+    } break;
+    case MOV_CH: {
+      moveRegister(C, H);
+    } break;
+    case MOV_CL: {
+      moveRegister(C, L);
+    } break;
+
+    case MOV_DA: {
+      moveRegister(D, A);
+    } break;
+    case MOV_DB: {
+      moveRegister(D, B);
+    } break;
+    case MOV_DC: {
+      moveRegister(D, C);
+    } break;
+    case MOV_DE: {
+      moveRegister(D, E);
+    } break;
+    case MOV_DH: {
+      moveRegister(D, H);
+    } break;
+    case MOV_DL: {
+      moveRegister(D, L);
+    } break;
+
+    case MOV_EA: {
+      moveRegister(E, A);
+    } break;
+    case MOV_EB: {
+      moveRegister(E, B);
+    } break;
+    case MOV_EC: {
+      moveRegister(E, C);
+    } break;
+    case MOV_ED: {
+      moveRegister(E, E);
+    } break;
+    case MOV_EH: {
+      moveRegister(E, H);
+    } break;
+    case MOV_EL: {
+      moveRegister(E, L);
+    } break;
+
+    case MOV_HA: {
+      moveRegister(H, A);
+    } break;
+    case MOV_HB: {
+      moveRegister(H, B);
+    } break;
+    case MOV_HC: {
+      moveRegister(H, C);
+    } break;
+    case MOV_HD: {
+      moveRegister(H, E);
+    } break;
+    case MOV_HE: {
+      moveRegister(H, E);
+    } break;
+    case MOV_HL: {
+      moveRegister(H, L);
+    } break;
+
+    case MOV_LA: {
+      moveRegister(L, A);
+    } break;
+    case MOV_LB: {
+      moveRegister(L, B);
+    } break;
+    case MOV_LC: {
+      moveRegister(L, C);
+    } break;
+    case MOV_LD: {
+      moveRegister(L, E);
+    } break;
+    case MOV_LH: {
+      moveRegister(L, H);
+    } break;
+    case MOV_LE: {
+      moveRegister(L, E);
+    } break;
+
+    // register indirect addressing mode
+    case MOV_AM: {
+      moveIndirectTo(A, mem, cycles);
+    } break;
+    case MOV_BM: {
+      moveIndirectTo(B, mem, cycles);
+    } break;
+    case MOV_CM: {
+      moveIndirectTo(A, mem, cycles);
+    } break;
+    case MOV_DM: {
+      moveIndirectTo(A, mem, cycles);
+    } break;
+    case MOV_EM: {
+      moveIndirectTo(E, mem, cycles);
+    } break;
+    case MOV_HM: {
+      moveIndirectTo(H, mem, cycles);
+    } break;
+    case MOV_LM: {
+      moveIndirectTo(L, mem, cycles);
+    } break;
+
+    case MOV_MA: {
+      moveIndirectFrom(A, mem, cycles);
+    } break;
+    case MOV_MB: {
+      moveIndirectFrom(B, mem, cycles);
+    } break;
+    case MOV_MC: {
+      moveIndirectFrom(A, mem, cycles);
+    } break;
+    case MOV_MD: {
+      moveIndirectFrom(A, mem, cycles);
+    } break;
+    case MOV_ME: {
+      moveIndirectFrom(E, mem, cycles);
+    } break;
+    case MOV_MH: {
+      moveIndirectFrom(H, mem, cycles);
+    } break;
+    case MOV_ML: {
+      moveIndirectFrom(L, mem, cycles);
+    } break;
+
+    // move immediate
+    case MVI_A: {
+      moveImmediate(A, cycles, mem);
+    } break;
+    case MVI_B: {
+      moveImmediate(B, cycles, mem);
+    } break;
+    case MVI_C: {
+      moveImmediate(C, cycles, mem);
+    } break;
+    case MVI_D: {
+      moveImmediate(D, cycles, mem);
+    } break;
+    case MVI_E: {
+      moveImmediate(E, cycles, mem);
+    } break;
+    case MVI_H: {
+      moveImmediate(H, cycles, mem);
+    } break;
+    case MVI_L: {
+      moveImmediate(L, cycles, mem);
+    } break;
+    case MVI_M: {
+      // memory address to where the data should be tranfered would be
+      // present in H-L register pair
+      auto address = readRegisterPair(H, L);
+      cycles--;
+      auto destination = mem[address];
+      moveImmediate(destination, cycles, mem);
+    } break;
+
+    // load store accumulator ( from register pair )
+    case LDAX_B: {
+      auto address = readRegisterPair(B, C);
+      cycles--;
+      A = mem[address];
+    } break;
+    case LDAX_D: {
+      auto address = readRegisterPair(D, E);
+      cycles--;
+      A = mem[address];
+    } break;
+    case STAX_B: {
+      auto address = readRegisterPair(B, C);
+      cycles--;
+      mem[address] = A;
+    } break;
+    case STAX_D: {
+      auto address = readRegisterPair(D, E);
+      cycles--;
+      mem[address] = A;
+    } break;
+
+    // load store accumulator directly
+    case LDA: {
+      loadAccumulator(cycles, mem);
+    } break;
+    case STA: {
+      storeAccumulator(cycles, mem);
+    }
+    default:
+      // throw for unknown instruction.
+      std::cerr << "unknown instruction\n";
+    }
+  }
+}
+
+/**
+ * Read the 16-bit (2-Bytes/1-Word) data from register pair reg1-reg2.
+ */
+Word CPU::readRegisterPair(const Byte &reg1, const Byte &reg2) {
+  // TODO: assert that reg1 and reg2 are valid register pair.
+  Word data = reg1 << 8;
+  data |= reg2;
+  return data;
+}
+
+/**
+ * Write 16-bit (2-Bytes/1-Word) data to register pair reg1-reg2.
+ */
+void CPU::writeRegisterPair(Byte &reg1, Byte &reg2, Word &data) {
+  // TODO: assert that reg1 and reg2 are valid register pair.
+  reg1 = data >> 8;
+  reg2 = static_cast <Byte> (data);
+}
+
+/**
+ * Fetch one byte data pointed by the Progam counter from memory MEM.
+ */
+Byte CPU::fetchByte(int &cycles, const Memory &mem) {
+  auto currentData = mem[pc];
+  --cycles;
+  ++pc;
+  return currentData;
+}
+
+/**
+ * Fetch one word ( 2-bytes ) data pointed by the Progam counter from
+ * memory MEM. Assuming the platform is little-endien, we would first
+ * find the lower byte of address then lower byte.
+ */
+Word CPU::fetchWord(int &cycles, const Memory &mem) {
+  Word currentData = mem[pc];
+  ++pc;
+  currentData |= (mem[pc] << 8);
+  cycles -= 2;
+  return currentData;
+}
+
+/** Instruction set */
+
+/**
+ * Load the content of accumulator (A register). The contents of a
+ * memory location, specified by a 16-bit address in the operand, are
+ * copied to the accumulator.
+ */
+void CPU::loadAccumulator(int &cycles, const Memory &mem) {
+  auto address = fetchWord(cycles, mem);
+  A = mem[address];
+}
+
+/**
+ * The contents of the accumulator are copied into the memory location
+ * specified by the operand.
+ * This is a 3-byte instruction, the second byte specifies the low-order
+ * address and the third byte specifies the high-order address.
+ */
+void CPU::storeAccumulator(int &cycles, Memory &mem) {
+  Word address = fetchWord(cycles, mem);
+  mem[address] = A;
+}
+
+/**
+ * Move data from SOURCE register to DESTINATION register.
+ * TODO: assert that both arguments are actually registers.
+ */
+void CPU::moveRegister(Byte &destination, Byte &source) {
+  source = destination;
+}
+
+/**
+ * Move the contents of the memory location pointed by the H-L pair to
+ * the DESTINATION
+ */
+void CPU::moveIndirectTo(Byte &destination, const Memory &mem, int &cycles) {
+  auto address = readRegisterPair(H, L);
+  cycles--;
+  destination = mem[address];
+}
+
+/**
+ * Move the contents of SOURCE register to the memory location pointed
+ * by the H-L pair.
+ */
+void CPU::moveIndirectFrom(const Byte &source, Memory &mem, int &cycles) {
+  auto address = readRegisterPair(H, L);
+  cycles--;
+  mem[address] = source;
+}
+
+/**
+ * Move the 8-bit(1-byte) data (provided as operand with the
+ * instruction) in the DESTINATION register or memory.
+ */
+void CPU::moveImmediate(Byte &destination, int &cycles, const Memory &mem) {
+  auto data = fetchByte(cycles, mem);
+  destination = data;
+}
--- src/cpu.h
+++ src/cpu.h
@@ -22,6 +22,8 @@ struct Memory {
   static constexpr unsigned MEM_LIMIT = 1024 * 64;
   std::array<Byte, MEM_LIMIT> data;
 
+  void initialise();
+
   // Read a byte
   Byte operator[](unsigned p_address) const { return data[p_address]; }
 
@@ -39,22 +41,87 @@ struct StatusFlags {
 
 class CPU {
 public:
-  CPU();
+  CPU() {}
   CPU(const CPU &other);
   CPU(CPU &&other) noexcept = delete;
-  virtual ~CPU() noexcept;
+  virtual ~CPU() noexcept {}
 
-  // Instruction set
+  void reset(Memory &mem);
+  void execute(int &cycles, Memory &mem);
 
-  // Debug
+  Word readRegisterPair(const Byte &reg1, const Byte &reg2);
+  void writeRegisterPair(Byte &reg1, Byte &reg2, Word &data);
+  Byte fetchByte(int &cycles, const Memory &mem);
+  Word fetchWord(int &cycles, const Memory &mem);
+
+  /** opcodes */
+
+  static constexpr Byte
+      /*  Data Transfer group */
+
+      // Move
+      MOV_AA = 0x7F,
+      MOV_AB = 0x78, MOV_AC = 0x79, MOV_AD = 0x7A, MOV_AE = 0x7B, MOV_AH = 0x7C,
+      MOV_AL = 0x7D, MOV_AM = 0x7E,
+
+      MOV_BA = 0x47, MOV_BB = 0x40, MOV_BC = 0x41, MOV_BD = 0x42, MOV_BE = 0x43,
+      MOV_BH = 0x44, MOV_BL = 0x45, MOV_BM = 0x46,
+
+      MOV_CA = 0x4F, MOV_CB = 0x48, MOV_CC = 0x49, MOV_CD = 0x4A, MOV_CE = 0x4B,
+      MOV_CH = 0x4C, MOV_CL = 0x4D, MOV_CM = 0x4E,
+
+      MOV_DA = 0x57, MOV_DB = 0x50, MOV_DC = 0x51, MOV_DD = 0x52, MOV_DE = 0x53,
+      MOV_DH = 0x54, MOV_DL = 0x55, MOV_DM = 0x56,
+
+      MOV_EA = 0x5F, MOV_EB = 0x58, MOV_EC = 0x59, MOV_ED = 0x5A, MOV_EE = 0x5B,
+      MOV_EH = 0x5C, MOV_EL = 0x5D, MOV_EM = 0x5E,
+
+      MOV_HA = 0x67, MOV_HB = 0x60, MOV_HC = 0x61, MOV_HD = 0x62, MOV_HE = 0x63,
+      MOV_HH = 0x64, MOV_HL = 0x65, MOV_HM = 0x66,
+
+      MOV_LA = 0x6F, MOV_LB = 0x68, MOV_LC = 0x69, MOV_LD = 0x6A, MOV_LE = 0x6B,
+      MOV_LH = 0x6C, MOV_LL = 0x6D, MOV_LM = 0x6E,
+
+      MOV_MA = 0x77, MOV_MB = 0x70, MOV_MC = 0x71, MOV_MD = 0x72, MOV_ME = 0x73,
+      MOV_MH = 0x74, MOV_ML = 0x75,
+
+      // Move immediate
+      MVI_A = 0x3E, MVI_B = 0x06, MVI_C = 0x0E, MVI_D = 0x16, MVI_E = 0x1E,
+      MVI_H = 0x26, MVI_L = 0x2E, MVI_M = 0x36,
+
+      // load immediate (reg pair)
+      LXI_B = 0x01, LXI_D = 0x11, LXI_H = 0x21, LXI_SP = 0x31,
+
+      // load/store A direct ( from register pair )
+      LDAX_B = 0x0A, LDAX_D = 0x1A, STAX_B = 0x02, STAX_D = 0x12,
+
+      // load/store A direct ( from memory )
+      LDA = 0x3A, STA = 0x32,
+
+      // load/store HL direct
+      LHLD = 0x3A, SHLD = 0x32;
+
+  /** instruction set */
+
+  void loadAccumulator(int &cycles, const Memory &mem);
+  void storeAccumulator(int &cycles, Memory &mem);
+
+  void moveRegister(Byte &destination, Byte &source);
+  void moveIndirectTo(Byte &destination, const Memory &mem, int &cycles);
+  void moveIndirectFrom(const Byte &source, Memory &mem, int &cycles);
+  void moveImmediate(Byte &destination, int &cycles, const Memory &mem);
+
+  /** debug */
+
+  /** getters */
+  auto getProgramCounter() const { return pc; }
 
 private:
-  StatusFlags flagRegister;
-  Word pc; // program counter
-  Word sp; // stack pointer
+  StatusFlags psw; // program status word
+  Word pc;         // program counter
+  Word sp;         // stack pointer
 
   Byte A, B, C, D, E, H, L;
-  Memory memory;
 };
 
 } // namespace emu
